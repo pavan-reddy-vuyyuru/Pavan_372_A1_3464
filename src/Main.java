69a69a608098781d0794a5946d844112152de9c7
Pavan Kumar Reddy Vuyyuru <pavansaivuyyyuru314@gmail.com>
2023-10-21 19:34:11 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worlgfgghjkhjggffd!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of triangles: ");
+        int numTriangles = scanner.nextInt();
+        scanner.nextLine();
+        if(numTriangles<3){
+            System.out.println("Please enter the value greater than 3 or equal to 3...");
+            return;
+        }
+
+        Triangle[] triangles = new Triangle[numTriangles];
+
+        for (int i = 0; i < numTriangles; i++) {
+            System.out.println("Enter the coordinates of the vertices for Triangle " + (i + 1));
+            Point[] vertices = new Point[3];
+
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Enter the Vertex X for triangle " + (j + 1) + ": ");
+                double x = scanner.nextDouble();
+                System.out.print("Enter the Vertex Y for triangle " + (j + 1) + ": ");
+                double y = scanner.nextDouble();
+                vertices[j] = new Point(x, y);
+            }
+
+            triangles[i] = new Triangle(vertices[0], vertices[1], vertices[2]);
+
+            Triangle triangle = triangles[i];
+            double perimeter = triangle.getPerimeter();
+            double area = triangle.calculateArea();
+            String classification = triangle.type();
+            System.out.println("Triangle " + (i + 1) + " - Perimeter: " + perimeter + ", Area: " + area + " , Type: " + classification);
+            System.out.print("Enter a point X to check if it's inside the triangle: ");
+            double x = scanner.nextDouble();
+            System.out.print("Enter a point Y to check if it's inside the triangle: ");
+            double y = scanner.nextDouble();
+            Point point = new Point(x, y);
+
+            if (triangle.isPointInside(point)) {
+                System.out.println("---The point is inside the triangle.---");
+            } else {
+                System.out.println("---The point is not inside the triangle.---");
+            }
+        }
     }
 }
+
--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Point {
+class Point {
+    double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point other) {
+        double dx = this.x - other.x;
+        double dy = this.y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+class Triangle {
+    Point[] vertices = new Point[3];
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        vertices[0] = p1;
+        vertices[1] = p2;
+        vertices[2] = p3;
+    }
+
+    public double getPerimeter()
+    {
+        double side1 = vertices[0].distanceTo(vertices[1]);
+        double side2 = vertices[1].distanceTo(vertices[2]);
+        double side3 = vertices[2].distanceTo(vertices[0]);
+        return side1 + side2 + side3;
+    }
+
+    public double calculateArea() {
+        double side1 = vertices[0].distanceTo(vertices[1]);
+        double side2 = vertices[1].distanceTo(vertices[2]);
+        double side3 = vertices[2].distanceTo(vertices[0]);
+        double s = (side1 + side2 + side3) / 2;
+        double area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        return area;
+    }
+
+    public boolean isPointInside(Point point) {
+        double areaTotal = calculateArea();
+        double area1 = new Triangle(point, vertices[1], vertices[2]).calculateArea();
+        double area2 = new Triangle(vertices[0], point, vertices[2]).calculateArea();
+        double area3 = new Triangle(vertices[0], vertices[1], point).calculateArea();
+
+        return areaTotal == (area1 + area2 + area3);
+    }
+
+    public String type() {
+        double side1 = vertices[0].distanceTo(vertices[1]);
+        double side2 = vertices[1].distanceTo(vertices[2]);
+        double side3 = vertices[2].distanceTo(vertices[0]);
+
+        if (side1 == side2 && side2 == side3) {
+            return "Equilateral";
+        } else if (side1 == side2 || side2 == side3 || side3 == side1) {
+            return "Isosceles";
+        } else {
+            return "Unknown";
+        }
+    }
 }
